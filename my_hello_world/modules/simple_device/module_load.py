427b53f210e0e244a790793123852f06f29bf254
cesardtamayo@gmail.com <cesar.tamayo@intel.com>
2023-04-19 11:07:35 -0700

    get_status() implemented

--- my_hello_world/modules/simple_device/module_load.py
+++ my_hello_world/modules/simple_device/module_load.py
@@ -10,7 +10,8 @@ class_name = 'simple_device'
 
 def get_info(obj):
     # USER-TODO: Return something useful here
-    return [("None")]
+    return [("Info",
+             [("Description", obj.class_desc)])]
 
 cli.new_info_command(class_name, get_info)
 
@@ -19,8 +20,21 @@ cli.new_info_command(class_name, get_info)
 #
 
 def get_status(obj):
-    # USER-TODO: Return something useful here
-    return [("Registers",
-             [("myregister", obj.regs_myregister)])]
+    # USER-TODO: Return something useful here    
+    return [("Attributes",
+                [
+                    ("reads_count", obj.reads_count),
+                    ("register_selection", obj.register_selection)
+                ]),
+            ("Register Bank", 
+                [
+                    ("inputRegister0", obj.regs_inputRegister0),
+                    ("inputRegister1", obj.regs_inputRegister1),
+                    ("inputRegister2", obj.regs_inputRegister2),
+                    ("inputRegister3", obj.regs_inputRegister3),
+                    ("outputRegister", obj.regs_outputRegister)
+                ]
+            )]
+                                  
 
 cli.new_status_command(class_name, get_status)
